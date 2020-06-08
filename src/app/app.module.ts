d24a522714bf7a6e7896b753e45455aa23579b46
jssmy <servidorlogico@gmail.com>
2020-06-08 03:46:46 +0000

    config

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderBottomComponent } from './shared/components/header-bottom/header-bottom.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/components/header-bottom/header-bottom.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HeaderBottomComponent} from './header-bottom.component';
+
+
+
+@NgModule({
+  declarations: [
+	  HeaderBottomComponent
+  ],
+  imports: [
+    CommonModule
+  ], export: [
+	  HeaderBottomComponent
+]
+})
+export class HeaderBottomModule { }
