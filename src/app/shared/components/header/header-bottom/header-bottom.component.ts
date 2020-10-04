f143e2abfcae1742410efca7ac68a2e635138c64
jssmy <joset.manihuari@telefonica.com>
2020-10-03 21:00:29 -0500

    styling

--- src/app/shared/components/header/header-bottom/header-bottom.component.ts
+++ src/app/shared/components/header/header-bottom/header-bottom.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Auth } from 'src/app/auth/auth.helper';
+import { AuthService } from 'src/app/auth/auth.service';
+import { RoutesPath } from 'src/app/shared/commons/constants/routes-path.enum';
 import { CookieHelper } from 'src/app/shared/commons/helpers/cookie-helper';
 import { IAuthUser } from 'src/app/shared/commons/interfaces/auth/iauth-user';
 
@@ -10,14 +13,19 @@ import { IAuthUser } from 'src/app/shared/commons/interfaces/auth/iauth-user';
 })
 export class HeaderBottomComponent implements OnInit {
   authCheck: boolean = Auth.check();
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
   logOut(event) {
     event.preventDefault();
+    this.authService.logout();
     CookieHelper.removeAll();
+    this.router.navigate([RoutesPath.MAIN]);
   }
 
 }
