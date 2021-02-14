f93a0bc9f267b432cf39ac83a97d18e0726842cc
jmanihuariy <joset.manihuari@telefonica.com>
2021-02-14 17:19:08 -0500

    change id app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './pages/layout/layout.module';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { LoaderService } from './shared/commons/interceptors/loader.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RequestService } from './shared/commons/interceptors/request.service';
 import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from 'angularx-social-login';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -43,7 +43,7 @@ import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthServiceConfig, So
           },
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('812388019564306'),
+            provider: new FacebookLoginProvider('469107014221930'),
           }
         ],
       } as SocialAuthServiceConfig,
